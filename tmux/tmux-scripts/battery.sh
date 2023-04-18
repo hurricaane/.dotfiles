21c36b2d8d1424d21f4fc13ab373943714f2d4ca
Yannick Dossou <yannick.dossou@octo.com>
2023-04-18 23:21:09 +0200

    tmux: configure tmux.conf

---
+++ tmux/tmux-scripts/battery.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+icon_battery='  '
+icon_charging='  '
+percentage=''
+battery_life=''
+
+systemctl status upower.service 1> /dev/null
+
+if [ $? -eq 0 ]; then
+  percentage=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
+  state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{print $2}')
+
+  if [ "$state" = "charging" ]; then
+    battery_life="${icon_charging} ${percentage}"
+  else
+    battery_life="${icon_battery} ${percentage}"
+  fi
+else
+  percentage=$(cat /sys/class/power_supply/BAT1/capacity)
+  battery_life="${icon_battery} ${percentage}%"
+fi
+
+echo "$battery_life"
