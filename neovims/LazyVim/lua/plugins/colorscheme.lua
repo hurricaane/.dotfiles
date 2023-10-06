2a2eff5a90c769275a524909a4aaa43138b39a3d
Yannick Dossou <yannick.dossou@octo.com>
2023-10-06 14:02:16 +0200

    nvim: add different colorschemes and change the default one

--- neovims/LazyVim/lua/plugins/colorscheme.lua
+++ neovims/LazyVim/lua/plugins/colorscheme.lua
@@ -1,12 +1,35 @@
 -- Manage colorschemes for LazyVim
 return {
   -- Add colorschemes
+  -- Kanagawa theme
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  -- Gruvbox theme
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+  -- Ayu theme
+  {
+    "Shatur/neovim-ayu",
+  },
+  -- Onedark theme
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+  },
+  -- Tokyodark theme
+  {
+    "tiagovla/tokyodark.nvim",
+  },
 
   -- Change the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "tokyodark",
     },
   },
 }
