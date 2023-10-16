b1501a977db2faee2d746a049290f4528ac23eb9
Yannick Dossou <yannick.dossou@octo.com>
2023-10-16 09:35:33 +0200

    nvim: add new cmp source

---
+++ neovims/LazyVim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "petertriho/cmp-git",
+    config = function()
+      require("cmp_git").setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "petertriho/cmp-git",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = false })
+
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "git" } }))
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-n>"] = cmp.mapping.scroll_docs(4),
+      })
+
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+}
