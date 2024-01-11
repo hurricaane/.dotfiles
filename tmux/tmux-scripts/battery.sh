f3d1395e687fbc28f5159464892fad551d9ccc82
Yannick Dossou <yannick.dossou@octo.com>
2024-01-11 16:49:54 +0100

    tmux: update status bar

--- tmux/tmux-scripts/battery.sh
+++ tmux/tmux-scripts/battery.sh
@@ -4,20 +4,20 @@ icon_charging='  '
 percentage=''
 battery_life=''
 
-systemctl status upower.service 1> /dev/null
+systemctl status upower.service 1>/dev/null
 
 if [ $? -eq 0 ]; then
-  percentage=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
-  state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{print $2}')
+	percentage=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
+	state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{print $2}')
 
-  if [ "$state" = "charging" ]; then
-    battery_life="${icon_charging} ${percentage}"
-  else
-    battery_life="${icon_battery} ${percentage}"
-  fi
+	if [ "$state" = "charging" ]; then
+		battery_life="${icon_charging} ${percentage}"
+	else
+		battery_life="${icon_battery} ${percentage}"
+	fi
 else
-  percentage=$(cat /sys/class/power_supply/BAT1/capacity)
-  battery_life="${icon_battery} ${percentage}%"
+	percentage=$(cat /sys/class/power_supply/BAT1/capacity)
+	battery_life="${icon_battery} ${percentage}%"
 fi
 
 echo "$battery_life"
