69876c9a13c0ce0f7d9b43bc1e4659b501456d81
Yannick Dossou <yannick.dossou@octo.com>
2024-02-22 16:40:49 +0000

    nvim: add correct config for vim-tmux-navigator

--- neovims/LazyVim/lua/config/keymaps.lua
+++ neovims/LazyVim/lua/config/keymaps.lua
@@ -6,10 +6,10 @@ vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC
 vim.keymap.set("i", "kj", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
 
 -- Overwrite LazyVim default mappings with vim-tmux-navigator mappings
-vim.keymap.set("n", "<C-h>", ":<C-U>TmuxNavigateLeft<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-j>", ":<C-U>TmuxNavigateDown<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", ":<C-U>TmuxNavigateUp<cr>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-h>", ":<C-U>TmuxNavigateLeft<cr>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-j>", ":<C-U>TmuxNavigateDown<cr>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-k>", ":<C-U>TmuxNavigateUp<cr>", { noremap = true, silent = true })
 
 -- Icon Picker keymap
 vim.keymap.set("n", "<leader>ip", "<cmd>IconPickerNormal<cr>", { desc = "Open Icon Picker and insert icon" })
--- neovims/LazyVim/lua/plugins/tmux.lua
+++ neovims/LazyVim/lua/plugins/tmux.lua
@@ -1,4 +1,18 @@
 -- Plugins for tmux integration with Neovim
 return {
   "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-tab>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
