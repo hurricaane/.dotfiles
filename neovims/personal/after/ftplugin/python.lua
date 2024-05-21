d706aeb41eb9e8472874890a425d1fbba2f37d7e
Yannick Dossou <yannick.dossou@octo.com>
2024-05-21 08:14:39 +0000

    feat(nvim|personal): add autocommand to load virtualenv when opening python files
    I don't know if it really works

--- neovims/personal/after/ftplugin/python.lua
+++ neovims/personal/after/ftplugin/python.lua
@@ -5,3 +5,20 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.fileformat = "unix"
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("yannick_python_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  group = augroup("venv"),
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
+})
