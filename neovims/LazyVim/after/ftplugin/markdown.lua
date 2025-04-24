801a2efc86dcf04ac41cb2932cf0ce53aa39dc60
Yannick Dossou <yannick.dossou@octo.com>
2025-04-24 16:19:47 +0200

    feat(nvim): add markdown configuration

--- neovims/LazyVim/after/ftplugin/markdown.lua
+++ neovims/LazyVim/after/ftplugin/markdown.lua
@@ -1,15 +1,4 @@
-local map = vim.keymap.set -- For conciseness
-
-map(
-  "n",
-  "<CR>",
-  "<Cmd>lua vim.lsp.buf.definition()<CR>",
-  { desc = "Open link under cursor", silent = true, noremap = true }
-)
-map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", { desc = "Notes referencing this note", silent = true, noremap = true })
-map(
-  "n",
-  "<leader>zl",
-  "<Cmd>ZkLinks<CR>",
-  { desc = "Notes being referenced by this note", silent = true, noremap = true }
-)
+-- Configuration specific to markdown files
+local option = vim.api.nvim_set_option_value
+option("colorcolumn", "81", {})
+option("textwidth", 80, {})
