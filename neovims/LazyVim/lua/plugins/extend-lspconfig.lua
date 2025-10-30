4cfb4ad87a352a497114608d8402520b1db49aef
Yannick Dossou <dossouyannick@gmail.com>
2025-10-30 14:15:42 +0100

    feat: update lazyvim, tmux and start wezterm config

--- neovims/LazyVim/lua/plugins/extend-lspconfig.lua
+++ neovims/LazyVim/lua/plugins/extend-lspconfig.lua
@@ -1,14 +1,23 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", false, mode = { "i" } }
-
-    -- customize yaml server
-    LazyVim.extend(opts.servers.yamlls, "settings.yaml.customTags", {
-      "!reference sequence",
-      "!reference mapping",
-      "!reference scalar",
-    })
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<C-k>", false, mode = { "i " } },
+        },
+      },
+      yamlls = {
+        settings = {
+          yaml = {
+            customTags = {
+              "!reference sequence",
+              "!reference mapping",
+              "!reference scalar",
+            },
+          },
+        },
+      },
+    },
+  },
 }
